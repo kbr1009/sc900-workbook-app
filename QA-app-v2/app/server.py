220b8b207c9d4b4e6170c4c26552b5c06b25dfaa
kbr1009 <reo109r@icloud.com>
2022-08-10 09:43:25 +0900

    大元のpdfファイルに遷移するようにリンクを追加した

--- QA-app-v2/app/server.py
+++ QA-app-v2/app/server.py
@@ -18,6 +18,24 @@ def a(id):
     fpath = endpoint + str(id+1) + '.pdf'
     return render_template('answer.html', fpath=fpath)
 
+@app.route('/order')
+def order():
+    fpath = 'https://lambda-s3-file.s3.ap-northeast-1.amazonaws.com/qa-app-v2/1.pdf'
+    return render_template('order.html', fpath=fpath, file_num=1)
+
+@app.route('/order/a/<int:id>')
+def order_a(id):
+    endpoint = 'https://lambda-s3-file.s3.ap-northeast-1.amazonaws.com/qa-app-v2/'
+    file_num = id +1
+    fpath = endpoint + str(file_num) + '.pdf'
+    return render_template('order_a.html', fpath=fpath, file_num=file_num)
+
+@app.route('/order/q/<int:id>')
+def order_q(id):
+    endpoint = 'https://lambda-s3-file.s3.ap-northeast-1.amazonaws.com/qa-app-v2/'
+    file_num = id +1
+    fpath = endpoint + str(file_num) + '.pdf'
+    return render_template('order_q.html', fpath=fpath, file_num=file_num)
 
 
 if __name__ == "__main__":
